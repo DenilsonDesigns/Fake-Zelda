855defcc96fa9b222b6ce7bb90d8907505371f29
DenilsonDesigns <daniellanguilller@gmail.com>
2025-07-23 20:52:07 +1000

    add sword animations

--- scenes/characters/link/link.gd
+++ scenes/characters/link/link.gd
@@ -1,6 +1,7 @@
 class_name Link extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+# @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @export var speed: float = 100.0
 
@@ -27,29 +28,26 @@ func _physics_process(_delta: float) -> void:
 func update_animation(dir: Vector2) -> void:
 	if dir == Vector2.ZERO:
 		if abs(last_move_dir.x) > abs(last_move_dir.y):
-			animated_sprite_2d.animation = "idle_side"
-			animated_sprite_2d.flip_h = last_move_dir.x < 0
+			$Sprite2D.flip_h = last_move_dir.x < 0
+			animation_player.play("idle_side")
 		elif last_move_dir.y > 0:
-			animated_sprite_2d.animation = "idle_down"
-			animated_sprite_2d.flip_h = false
+			$Sprite2D.flip_h = false
+			animation_player.play("idle_down")
 		else:
-			animated_sprite_2d.animation = "idle_up"
-			animated_sprite_2d.flip_h = false
-
-		animated_sprite_2d.play()
+			$Sprite2D.flip_h = false
+			animation_player.play("idle_up")
 		return
 
 	if abs(dir.x) > abs(dir.y):
-		animated_sprite_2d.animation = "walk_side"
-		animated_sprite_2d.flip_h = dir.x < 0
+		$Sprite2D.flip_h = dir.x < 0
+		animation_player.play("walk_side")
 	elif dir.y > 0:
-		animated_sprite_2d.animation = "walk_down"
-		animated_sprite_2d.flip_h = false
+		$Sprite2D.flip_h = false
+		animation_player.play("walk_down")
 	else:
-		animated_sprite_2d.animation = "walk_up"
-		animated_sprite_2d.flip_h = false
+		$Sprite2D.flip_h = false
+		animation_player.play("walk_up")
 
-	animated_sprite_2d.play()
 
 func play_sword_animation():
 	is_attacking = true
@@ -57,14 +55,12 @@ func play_sword_animation():
 	var anim_name := ""
 	if abs(last_move_dir.x) > abs(last_move_dir.y):
 		anim_name = "swing_side"
-		animated_sprite_2d.flip_h = last_move_dir.x < 0
+		$Sprite2D.flip_h = last_move_dir.x < 0
 	elif last_move_dir.y > 0:
 		anim_name = "swing_down"
 	else:
 		anim_name = "swing_up"
 
-	animated_sprite_2d.play(anim_name)
-
-	await animated_sprite_2d.animation_finished
-
+	animation_player.play(anim_name)
+	await animation_player.animation_finished
 	is_attacking = false
--- scenes/environment/shrub.gd
+++ scenes/environment/shrub.gd
@@ -1,10 +1,12 @@
 extends StaticBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision_shape_2d: CollisionShape2D = $HitBox/CollisionShape2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var hit_box: Area2D = $HitBox
+@onready var hit_box_shape: CollisionShape2D = $HitBox/HitBoxShape
 
 func _ready() -> void:
-	$HitBox.connect("area_entered", _on_hit_box_area_entered)
+	hit_box.connect("area_entered", _on_hit_box_area_entered)
 
 func _on_hit_box_area_entered(area: Area2D) -> void:
 	if area.name == "SwordHitBox":
@@ -12,6 +14,7 @@ func _on_hit_box_area_entered(area: Area2D) -> void:
 
 func die():
 	animated_sprite_2d.play("destroy")
-	collision_shape_2d.disabled = true
-	$HitBox.set_deferred("monitoring", false)
+	hit_box_shape.disabled = true
+	hit_box.set_deferred("monitoring", false)
+	collision_shape_2d.set_deferred("disabled", true)
 	await animated_sprite_2d.animation_finished
