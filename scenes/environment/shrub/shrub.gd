6288e2d25a148205976460a7c46e61f012a33d9c
DenilsonDesigns <daniellanguilller@gmail.com>
2025-07-24 20:22:18 +1000

    shrub killable and y-sorted

--- scenes/environment/shrub/shrub.gd
+++ scenes/environment/shrub/shrub.gd
@@ -5,7 +5,13 @@ extends StaticBody2D
 @onready var hit_box: Area2D = $HitBox
 @onready var hit_box_shape: CollisionShape2D = $HitBox/HitBoxShape
 
+@export var dead_shrubs_container: Node2D
+
 func _ready() -> void:
+	if not dead_shrubs_container:
+		assert(dead_shrubs_container, "dead_shrub_container required")
+		queue_free()
+		return
 	hit_box.connect("area_entered", _on_hit_box_area_entered)
 
 func _on_hit_box_area_entered(area: Area2D) -> void:
@@ -18,3 +24,7 @@ func die():
 	hit_box.set_deferred("monitoring", false)
 	collision_shape_2d.set_deferred("disabled", true)
 	await animated_sprite_2d.animation_finished
+
+	get_parent().remove_child(self)
+	dead_shrubs_container.add_child(self)
+	global_position = global_position
