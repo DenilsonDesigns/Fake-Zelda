7a33346290956b22eb03d12e4614d322ff6a8489
DenilsonDesigns <daniellanguilller@gmail.com>
2025-07-26 15:51:25 +1000

    knight can follow link, plus movement refactors

--- scenes/characters/knight/knight.gd
+++ scenes/characters/knight/knight.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 @onready var head_turn_timer: Timer = $HeadTurnTimer
+@onready var agro_area: Area2D = $AgroArea
+@onready var link: Node2D = null
 
 @export var min_walk_cycle: int = 2
 @export var max_walk_cycle: int = 6
@@ -17,6 +19,8 @@ var head_direction := ""
 var movement_direction := "down"
 
 func _ready() -> void:
+	agro_area.connect("body_entered", _on_agro_area_body_entered)
+
 	walk_cycles = randi_range(min_walk_cycle, max_walk_cycle)
 	speed = randf_range(min_speed, max_speed)
 	set_movement_target()
@@ -24,6 +28,19 @@ func _ready() -> void:
 	head_turn_timer.start()
 
 func _physics_process(_delta: float) -> void:
+	if link:
+		follow_link()
+	else:
+		patrol()
+
+func follow_link() -> void:
+	var link_direction = (link.global_position - global_position).normalized()
+	velocity = link_direction * speed
+	animated_sprite_2d.flip_h = velocity.x < 0
+	move_and_slide()
+	handle_animation()
+
+func patrol() -> void:
 	if navigation_agent_2d.is_navigation_finished():
 		current_walk_cycle += 1
 		if current_walk_cycle < walk_cycles:
@@ -47,6 +64,13 @@ func _physics_process(_delta: float) -> void:
 	else:
 		play_idle_animation(movement_direction)
 
+func handle_animation() -> void:
+	if velocity.length() > 0.1:
+		movement_direction = get_movement_direction(velocity)
+		play_walk_animation(movement_direction)
+	else:
+		play_idle_animation(movement_direction)
+
 func get_movement_direction(vel: Vector2) -> String:
 	if abs(vel.x) > abs(vel.y):
 		return "side"
@@ -55,12 +79,12 @@ func get_movement_direction(vel: Vector2) -> String:
 	else:
 		return "down"
 
-func play_walk_animation(dir: String):
+func play_walk_animation(dir: String) -> void:
 	var anim_name = "walk_" + dir + head_direction
 	if animated_sprite_2d.animation != anim_name:
 		animated_sprite_2d.play(anim_name)
 
-func play_idle_animation(dir: String):
+func play_idle_animation(dir: String) -> void:
 	var anim_name = "idle_" + dir + head_direction
 	if animated_sprite_2d.animation != anim_name:
 		animated_sprite_2d.play(anim_name)
@@ -75,3 +99,8 @@ func _on_head_turn_timer_timeout() -> void:
 	head_direction = options[randi() % options.size()]
 	head_turn_timer.wait_time = randf_range(2.0, 4.0)
 	head_turn_timer.start()
+
+
+func _on_agro_area_body_entered(body: Node2D) -> void:
+	if body.name == "Link":
+		link = body
