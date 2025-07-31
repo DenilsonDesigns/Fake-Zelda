ee6c761f49ec0a048baac4c2ce526fcea06d233a
DenilsonDesigns <daniellanguilller@gmail.com>
2025-07-31 22:21:58 +1000

    add game state script

---
+++ scripts/autoload/game_state.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var rupee_balance: int = 0
+
+signal rupee_balance_changed(new_balance: int)
+
+func add_rupees(amount: int) -> void:
+	rupee_balance += amount
+	rupee_balance_changed.emit(rupee_balance)
+
+# @TODO: add to Link and UI scenes
