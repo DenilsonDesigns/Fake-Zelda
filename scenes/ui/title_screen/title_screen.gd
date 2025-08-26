6ab40d0b3ec58c08ed235e37da2073fceb90f361
DenilsonDesigns <daniellanguilller@gmail.com>
2025-08-26 22:44:36 +1000

    add title screen with animations

--- scenes/ui/title_screen/title_screen.gd
+++ scenes/ui/title_screen/title_screen.gd
@@ -1,3 +1,34 @@
 class_name TitleScreen extends Control
 
-# @TODO: incoming:
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var sparkle_anims := [
+	"sparkle_bottom_left",
+	"sparkle_bottom_right",
+	"sparkle_top_right",
+	"sparkle_top_left"
+]
+
+var last_sparkle: String = ""
+
+func _ready() -> void:
+	animated_sprite_2d.play("sword_drop")
+	animated_sprite_2d.animation_finished.connect(_on_animated_sprite_2d_animation_finished)
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "sword_drop":
+		_play_random_sparkle()
+
+func _play_random_sparkle() -> void:
+	var anim = sparkle_anims.pick_random()
+
+	while anim == last_sparkle:
+		anim = sparkle_anims.pick_random()
+
+	last_sparkle = anim
+
+	animated_sprite_2d.play(anim)
+	animated_sprite_2d.animation_finished.connect(_on_sparkle_finished, ConnectFlags.CONNECT_ONE_SHOT)
+
+func _on_sparkle_finished() -> void:
+	_play_random_sparkle()
